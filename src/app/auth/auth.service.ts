ba05a2ee4b2045f0ac935f5a83b6360106ce659b
justJosh1004 <j.joson@live.com>
2019-07-23 11:09:26 -0400

    added token timeout in UI

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class AuthService {
   private isAuthenticated = false;
   private token: string;
+  private tokenTimer: any;
   private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) {}
@@ -38,7 +39,7 @@ export class AuthService {
   login(email: string, password: string) {
     const authData: AuthData = { email, password };
     this.http
-      .post<{ token: string }>(
+      .post<{ token: string; expiresIn: number }>(
         'http://localhost:3000/api/users/login',
         authData
       )
@@ -46,6 +47,10 @@ export class AuthService {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(['/']);
@@ -57,6 +62,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(['/']);
   }
 }
